770962ef5df0f72f49f9e7f97f53b6063b100d64
Dpag <dpag@pjf.mg.gov.br>
2025-01-07 15:35:07 -0300

    feat: adicionado login automatizado

--- index.py
+++ index.py
@@ -5,3 +5,23 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
 navegador = webdriver.Chrome()
+
+login = '12801230600'
+
+senha = '123456'
+
+planilha_convercao = 0
+
+dados_convercao = 0
+
+dados_insercao = 0
+
+navegador.get('https://juizdefora-mg.vivver.com/ram/ppi/definicao_pactuacao')
+time.sleep(2)
+
+login = navegador.find_element(By.XPATH, '//*[@id="conta"]').send_keys(login)
+senha = navegador.find_element(By.XPATH, '//*[@id="password"]').send_keys(senha)
+botao_login = navegador.find_element(By.XPATH, '/html/body/div[1]/div/div/form/div[2]').click()
+
+while len(navegador.find_elements(By.XPATH,'//*[@id="lookup_key_ppi_definicao_pactuacao_ram_lista_municipio_sede_id"]')) < 1:
+    time.sleep(1)
