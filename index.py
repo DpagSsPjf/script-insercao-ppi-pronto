ac6c25667325d0302caa8168c5c8c196daba7f62
Dpag <dpag@pjf.mg.gov.br>
2025-01-07 14:52:38 -0300

    commit inicial

---
+++ index.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+import pandas as pd
+navegador = webdriver.Chrome()
