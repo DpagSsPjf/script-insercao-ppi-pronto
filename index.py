41735f7b0e38e0cd494a98966b6753de1144fe53
Diogo Alberto <diogoalberto.dev@gmail.com>
2025-01-09 16:42:36 -0300

    fix: adicionado veirificacao se especialidade_cbo é nan, caso seja nao tenta inserir

--- index.py
+++ index.py
@@ -14,8 +14,7 @@ senha = '123456'
 
 #planilha_dados = 'dados_insercao.xlsx'
 
-colunas_convercao = ['cod_unico_sisreg','cod_interno_sisreg','cod_procedimento',
-                     'cod_CBO','esp_CBO']
+colunas_convercao = ['cod_unico_sisreg','cod_interno_sisreg','cod_procedimento','cod_CBO','esp_CBO']
 
 planilha_convercao = 'dados_convercao.xlsx'
 
@@ -23,7 +22,7 @@ df = pd.read_excel(planilha_convercao, dtype={coluna: str for coluna in colunas_
 
 #dados_insercao = pd.read_excel(planilha_dados)
 
-procedimento_filtrado = df.loc[df['cod_interno_sisreg'] == '9000024'].values.flatten()
+procedimento_filtrado = df.loc[df['cod_interno_sisreg'] == '0701483'].values.flatten()
 
 print(procedimento_filtrado)
 
@@ -43,25 +42,27 @@ botao_login = navegador.find_element(By.XPATH, '/html/body/div[1]/div/div/form/d
 while len(navegador.find_elements(By.XPATH,'//*[@id="lookup_key_ppi_definicao_pactuacao_ram_lista_municipio_sede_id"]')) < 1:
     time.sleep(1)
 
-municipio_sede = navegador.find_element(By.XPATH, '//*[@id="lookup_key_ppi_definicao_pactuacao_ram_lista_municipio_sede_id"]').send_keys('2')
+def inserir_dados_padroes():
+    municipio_sede = navegador.find_element(By.XPATH, '//*[@id="lookup_key_ppi_definicao_pactuacao_ram_lista_municipio_sede_id"]').send_keys('2')
 
-ano = navegador.find_element(By.XPATH, '//*[@id="ppi_definicao_pactuacao_ano"]').send_keys('2025')
+    ano = navegador.find_element(By.XPATH, '//*[@id="ppi_definicao_pactuacao_ano"]').send_keys('2025')
 
-mes = navegador.find_element(By.XPATH, '//*[@id="div_dados_pactuacao"]/div[9]/div[3]/div/span/span[1]/span/span[2]/b').click()
+    mes = navegador.find_element(By.XPATH, '//*[@id="div_dados_pactuacao"]/div[9]/div[3]/div/span/span[1]/span/span[2]/b').click()
 
-mes_input = navegador.find_element(By.XPATH, '/html/body/span/span/span[1]/input').send_keys('janeiro')
+    mes_input = navegador.find_element(By.XPATH, '/html/body/span/span/span[1]/input').send_keys('janeiro')
 
-acoes.send_keys(Keys.ENTER).perform()
+    acoes.send_keys(Keys.ENTER).perform()
 
-camp_procedimento = navegador.find_element(By.XPATH, '//*[@id="lookup_key_ppi_definicao_pactuacao_adm_procedimento_id"]').send_keys(procedimento)
+def inserir_dados_variaveis(cod_cbo):
+    camp_procedimento = navegador.find_element(By.XPATH, '//*[@id="lookup_key_ppi_definicao_pactuacao_adm_procedimento_id"]').send_keys(procedimento)
 
-time.sleep(0.3)
+    time.sleep(0.3)
 
-camp_cbo = navegador.find_element(By.XPATH, '//*[@id="lookup_key_ppi_definicao_pactuacao_adm_especialidade_id"]').send_keys(cbo)
+    camp_cbo = navegador.find_element(By.XPATH, '//*[@id="lookup_key_ppi_definicao_pactuacao_adm_especialidade_id"]').send_keys(cbo)
 
-time.sleep(0.3)
+    time.sleep(0.3)
 
-if math.isnan(especialidade_cbo):
-    camp_especialidade_cbo = navegador.find_element(By.XPATH, '//*[@id="lookup_key_ppi_definicao_pactuacao_cbo_especialidade_id"]').send_keys(especialidade_cbo)
+    if math.isnan(int(especialidade_cbo)) == False:
+        camp_especialidade_cbo = navegador.find_element(By.XPATH, '//*[@id="lookup_key_ppi_definicao_pactuacao_cbo_especialidade_id"]').send_keys(especialidade_cbo)
 
-time.sleep(3)
+    time.sleep(3)
